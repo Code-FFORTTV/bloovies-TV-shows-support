88984db7a3bb695e40cbc9ba87259f4aa56ce791
Albinus <99928204+albibos@users.noreply.github.com>
2023-04-16 06:21:15 +0000

    dfs

--- script.js
+++ script.js
@@ -3,29 +3,35 @@ const episodeId = urlParams.get("episode");
 const mediaId = urlParams.get("show");
 
 if (document.getElementById("blazer") && document.getElementById('blazer').innerHTML === 'Official <span>Blazer</span> Service') {
-    if (episodeId && mediaId) {
-        document.getElementById('content').style.display = 'none'
+  if (episodeId && mediaId) {
+    document.getElementById('content').style.display = 'none';
 
-        var video = document.getElementById('player');
-        video.style.display = 'block';
-        var hls = new Hls();
-        fetch(`https://api.consumet.org/movies/flixhq/watch?episodeId=${episodeId}&mediaId=${mediaId}`)
-            .then(response => response.json())
-            .then(data => {
-                hls.loadSource(data.sources[0].url);
-                hls.attachMedia(video);
-                hls.on(Hls.Events.MANIFEST_PARSED, function () {
-                    video.play();
-                });
-            });
+    var video = document.getElementById('player');
+    video.style.display = 'block';
+    var hls = new Hls();
+    fetch(`https://api.consumet.org/movies/flixhq/watch?episodeId=${episodeId}&mediaId=${mediaId}`)
+      .then(response => response.json())
+      .then(data => {
+        hls.loadSource(data.sources[0].url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+          video.play();
+        });
 
-        const player = new Plyr(video);
-    }
-    document.querySelector('#location').innerHTML = window.location.href + '?movie=watch-puss-in-boots-the-last-wish-91342';
-    document.querySelector('#location').onclick = function() {
-        window.open("/?movie=watch-puss-in-boots-the-last-wish-91342", "_blank");
-    }
+        if (data.subtitles && data.subtitles.length > 0) {
+          var subtitleTrack = document.getElementById('subtitleTrack');
+          subtitleTrack.src = data.subtitles[0].url;
+        }
+      });
+
+
+    const player = new Plyr(video);
+  }
+  document.querySelector('#location').innerHTML = window.location.href + '?movie=watch-puss-in-boots-the-last-wish-91342';
+  document.querySelector('#location').onclick = function() {
+    window.open("/?movie=watch-puss-in-boots-the-last-wish-91342", "_blank");
+  }
 } else {
-    alert('Not an Official service')
-    document.body.innerHTML = ''
+  alert('Not an Official service')
+  document.body.innerHTML = ''
 }
