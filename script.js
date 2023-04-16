972e3b556a9df507f45faa497f86b1358e25c5e2
Albinus <99928204+albibos@users.noreply.github.com>
2023-04-16 03:48:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const urlParams = new URLSearchParams(window.location.search);
+const episodeId = urlParams.get("episode");
+const mediaId = urlParams.get("show");
+
+if (document.getElementById("blazer") && document.getElementById('blazer').innerHTML === 'Official <span>Blazer</span> Service') {
+    if (episodeId && mediaId) {
+        document.getElementById('content').style.display = 'none'
+
+        var video = document.getElementById('player');
+        video.style.display = 'block';
+        var hls = new Hls();
+        fetch(`https://api.consumet.org/movies/flixhq/watch?episodeId=${episodeId}&mediaId=${mediaId}`)
+            .then(response => response.json())
+            .then(data => {
+                hls.loadSource(data.sources[0].url);
+                hls.attachMedia(video);
+                hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                    video.play();
+                });
+            });
+
+        const player = new Plyr(video);
+    }
+    document.querySelector('#location').innerHTML = window.location.href + '?movie=watch-puss-in-boots-the-last-wish-91342';
+    document.querySelector('#location').onclick = function() {
+        window.open("/?movie=watch-puss-in-boots-the-last-wish-91342", "_blank");
+    }
+} else {
+    alert('Not an Official service')
+    document.body.innerHTML = ''
+}
